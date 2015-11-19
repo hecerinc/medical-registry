1545925a1cc5703ef2a1c66aeb07a95f44fa215d
Hector Rincon <hecerinc@hotmail.com>
2015-11-18 23:57:28 -0600

    Added consultas & seperated RM from SQL.

---
+++ consultas.sql
@@ -0,0 +1,10 @@
+# ---------------------------------------------
+# 
+#		Documento para poner sus consultas
+# 	
+#
+#	1. Fork the repo
+#   2. Add your queries here
+#	3. Create pull request
+# ---------------------------------------------
+
