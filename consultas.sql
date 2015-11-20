f1e95f4d4cd0257cdc30f4353e3dc159b0f645a5
Hector Rincon <hecerinc@hotmail.com>
2015-11-20 10:17:22 -0600

    Added consultas

--- consultas.sql
+++ consultas.sql
@@ -8,3 +8,6 @@
 #	3. Create pull request
 # ---------------------------------------------
 
+SELECT date, exam, test FROM patients JOIN labtests ON patients.ssid = labtests.patient_id JOIN labtest_catalog ON labtests.test_id = labtest_catalog.id WHERE ssid = 280580223;
+
+SELECT date, fname, lname, parameter, value, min, max, comments FROM labtests JOIN results ON labtest_id = labtests.id JOIN patients ON patient_id = patients.ssid WHERE labtests.id = 2;
